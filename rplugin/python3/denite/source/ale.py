213a27b90846d4dc2ac6deabca7d4520c92c383b
iyuuya <i.yuuya@gmail.com>
2017-10-04 04:37:31 +0900

    implements denite ale source

---
+++ rplugin/python3/denite/source/ale.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from .base import Base
+
+
+class Source(Base):
+
+    def __init__(self, vim):
+        super().__init__(vim)
+        self.name = 'ale'
+        self.kind = 'file'
+
+    def on_init(self, context):
+        context['__bufnr'] = str(self.vim.call('bufnr', '%'))
+
+    def gather_candidates(self, context):
+        loclist = self.vim.call('ale#engine#GetLoclist', context['__bufnr'])
+        return [self._convert(loc) for loc in loclist]
+
+    def _convert(self, locinfo):
+        return {
+                'word' : locinfo['text'],
+                'abbr' : '[%d:%d] %s' % (locinfo['lnum'], locinfo['col'], locinfo['text']),
+                'action__path' : self.vim.call('bufname', locinfo['bufnr']),
+                'action__line' : locinfo['lnum'],
+                'action__col' : locinfo['col']
+        }
